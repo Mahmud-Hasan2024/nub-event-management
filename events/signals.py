1053217e5125348be68be1768b7c1913861c4b2b
Mahmud Hasan <mh.kid234@gmail.com>
2025-10-17 10:48:51 +0600

    updated events.signals.py to run asynchrously

--- events/signals.py
+++ events/signals.py
@@ -1,21 +1,27 @@
+from threading import Thread
+from django.core.mail import send_mail
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 from events.models import Event
 
 User = get_user_model()
 
+def send_email_async(subject, message, from_email, recipient_list):
+    Thread(
+        target=send_mail,
+        args=(subject, message, from_email, recipient_list),
+        kwargs={'fail_silently': True}
+    ).start()
 
 @receiver(m2m_changed, sender=Event.participants.through)
 def send_rsvp_email(sender, instance, action, pk_set, **kwargs):
     if action == "post_add":
         for user_id in pk_set:
             user = User.objects.get(pk=user_id)
-            send_mail(
+            send_email_async(
                 subject="RSVP Confirmation",
                 message=f"Hi {user.username}, you've successfully RSVPed to the event '{instance.name}'.",
                 from_email="lboy71943@gmail.com",
-                recipient_list=[user.email],
-                fail_silently=True,
+                recipient_list=[user.email]
             )
